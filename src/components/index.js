cc17488e6a1e2b3627ae4dbbe618ac814687a0f9
Adetoye daniel kehinde <adetoyedaniel2023@gmail.com>
2024-03-18 00:09:16 +0100

    first commit

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,6 @@ import SubHeading from "./SubHeading/SubHeading";
 import Newsletter from "./Footer/Newsletter";
 import FooterOverlay from "./Footer/FooterOverlay";
 import MenuItem from "./Menuitem/MenuItem";
-import Navbar from "./Navbar/Navbar";
+import Navbar from "./navbar/Navbar";
 
 export { SubHeading, Newsletter, FooterOverlay, MenuItem, Navbar };
